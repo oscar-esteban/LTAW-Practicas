8be8c3979c4f8d85ea7f7fe9237f1180223fb2a2
oscar-esteban <oscar.esteban.97@gmail.com>
2022-04-19 13:15:22 +0200

    javascript tienda

--- P2/tienda.js
+++ P2/tienda.js
@@ -1,25 +1,19 @@
-//-- Imprimir información sobre la solicitud recibida
 
 const http = require('http');
 const fs = require('fs');
 const PUERTO = 8080;
 
-//-- Npmbre del fichero JSON a leer
+
 const FICHERO_JSON = "tienda.json"
-//-- Leer el fichero JSON
 const  tienda_json = fs.readFileSync(FICHERO_JSON);
-//-- Crear la estructura tienda a partir del contenido del fichero
 const tienda = JSON.parse(tienda_json);
 
-const tienda2 = "tienda2.json"
 
-//-- Cargar pagina web del formulario
 const FORMULARIO = fs.readFileSync('form1.html','utf-8');
-//-- HTML de la página de respuesta
 const RESPUESTA = fs.readFileSync('pagina1/tienda.html', 'utf-8');
 
 
-//-- SERVIDOR: Bucle principal de atención a clientes
+
 const server = http.createServer((req, res) => {
 
   //-- Construir el objeto url con la url de la solicitud
@@ -32,25 +26,18 @@ const server = http.createServer((req, res) => {
   console.log("  Ruta: " + myURL.pathname);
   console.log("  Parametros: " + myURL.searchParams);
   
-  let page = ""; //pagina que queremos cargar.
-  
-  let myJSON = JSON.stringify(tienda);
-  fs.writeFileSync(tienda2, myJSON);
-  //-- Por defecto entregar formulario
-  let content_type = "text/html";
-  let content = FORMULARIO;
-
+  let page = ''; //pagina que queremos cargar.
   if (myURL.pathname == '/procesar') { // 1OSCAR
     let nombre = myURL.searchParams.get('nombre'); 
     let correo = myURL.searchParams.get('correo');
-      content_type = "text/html";
-      content = RESPUESTA;
-      let userExist = false;
-        for (i = 0; i < tienda.usuarios.length; i++) {
-            if (nombre == tienda.usuarios[i].nombre && password == tienda.usuarios[i].correo) {
-                userExist = true
-            }
+      
+    let userExist = false;
+      for (i = 0; i < tienda.usuarios.length; i++) {
+        if (nombre == tienda.usuarios[i].nombre && password == tienda.usuarios[i].correo) {
+          userExist = true
         }
+      }
+      
       if (userExist == true) {
         //-- Asignar la cookie de usuario 
         res.setHeader('Set-Cookie', "user=" + nombre);
