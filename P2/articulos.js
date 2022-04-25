7eadd003c18405f77685f20bbd5374062684e233
oscar-esteban <oscar.esteban.97@gmail.com>
2022-04-25 12:29:01 +0200

    articulos p2

---
+++ P2/articulos.js
@@ -0,0 +1,9 @@
+function main() {
+    var boton_rupee1 = document.getElementById("rupia1");
+    var boton_rupee2 = document.getElementById("rupia2");
+    var boton_rupee3 = document.getElementById("rupia3");
+    var botoncarro = document.getElementById("carro")
+
+
+   
+}
